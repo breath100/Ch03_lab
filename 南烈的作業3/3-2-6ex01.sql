0f6bcd5b8f55c4e979302f0fc8d715a8eabf8d29
breath100 <113489083+breath100@users.noreply.github.com>
2023-10-10 00:59:10 +0800

    Delete 南烈的作業3 directory

--- 南烈的作業3/3-2-6ex01.sql
+++
@@ -1,4 +0,0 @@
-Insert Into Employee
-Values('S0006','六合','D010')
-
---子關聯表的部碼(外鍵)無法參考父關聯表的部碼
--- 南烈的作業3/3-3-1_query.sql
+++
@@ -1,3 +0,0 @@
-SELECT  Employee2.Em_id AS Expr1, Employee2.Em_name, performance.*
-FROM     Employee2 INNER JOIN
-               performance ON Employee2.Em_id = performance.Em_id
